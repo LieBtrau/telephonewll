38aa3fd75d479c12816b5783ae39aaca60338ac1
prog.send@gmail.com <prog.send@gmail.com@cfb029ab-047b-d5c6-4fa8-ed4fa5c59d16>
2013-05-17 20:03:13 +0000

    wrong commit

--- tags/trunk/software/phoneShield/g711.h
+++
@@ -1,6 +0,0 @@
-#ifndef G711_H
-#define G711_H
-
-byte aLawEncode(int pcm);
-int16_t aLawDecode(byte alaw);
-#endif
--- tags/trunk/software/phoneShield/pcm.h
+++
@@ -1,7 +0,0 @@
-#ifndef PCM_H
-#define PCM_H
-
-void setupPcmGateway();
-void controlPcmGateway(boolean bOn);
-byte PCMcomm(byte data);
-#endif
